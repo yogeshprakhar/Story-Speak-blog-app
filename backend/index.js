6f92b2d2af4b40ab84577e83c9153559ad1c33da
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-05-19 16:02:03 +0530

    connect database and crate user model and test api

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Database is connected"))
+  .catch(() => console.log("error while connecting database"));
 
 const app = express();
 
 app.listen(9000, () => {
   console.log("Server running at port 9000");
 });
+
+app.use("/api/user", userRoutes);

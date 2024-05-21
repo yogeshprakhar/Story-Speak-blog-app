3555448739664be0bf556d135592b7512e4e1e58
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-05-21 19:21:57 +0530

    complet sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
@@ -18,7 +19,10 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
+          <Route element={<PrivateRoute/>}>
+
           <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
           <Route path="/project" element={<Projects />} />
         </Routes>
         <Footer/>
---
+++ frontend/src/components/DashSidebar.jsx
@@ -0,0 +1,37 @@
+import { Sidebar } from "flowbite-react";
+import React from "react";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const DashSidebar = () => {
+    const location = useLocation();
+    const [tab, setTab] = useState("");
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const tabFromUrl = urlParams.get("tab");
+      if (tabFromUrl) {
+        setTab(tabFromUrl);
+      }
+    }, [location.search]);
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor="dark">
+            Profile
+          </Sidebar.Item>
+          <Sidebar.Item
+            classname="cursor-pointer"
+            icon={HiArrowSmRight}
+            label={"user"}
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+};
+
+export default DashSidebar;

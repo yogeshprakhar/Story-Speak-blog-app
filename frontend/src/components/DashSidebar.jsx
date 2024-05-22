8983757eef645983b27f648fe5fb22bd6f6f35e3
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-05-22 16:41:36 +0530

    add users page for admin with functionality

--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -1,6 +1,11 @@
 import { Sidebar } from "flowbite-react";
 import React from "react";
-import { HiUser, HiArrowSmRight, HiDocumentText } from "react-icons/hi";
+import {
+  HiUser,
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+} from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
@@ -62,7 +67,17 @@ const DashSidebar = () => {
               </Sidebar.Item>
             </Link>
           )}
-
+          {currentUser.isAdmin && (
+            <Link to={"/dashboard?tab=users"}>
+              <Sidebar.Item
+                active={tab === "users"}
+                icon={HiOutlineUserGroup}
+                as="div"
+              >
+                Users
+              </Sidebar.Item>
+            </Link>
+          )}
           <Sidebar.Item
             className="cursor-pointer"
             icon={HiArrowSmRight}

39d534c4567b6236296eea0f5d96dc1c19cc46dd
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-05-22 15:47:39 +0530

    create get posts api route

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,13 +1,17 @@
-import express from "express"
-import {verifyToken} from "../utils/verifyUser.js"
-import { create } from "../controllers/post.controller.js";
+import express from "express";
+import { verifyToken } from "../utils/verifyUser.js";
+import {
+  create,
+  getposts,
+  deletepost,
+  updatepost,
+} from "../controllers/post.controller.js";
 
-const router = express.Router()
-
+const router = express.Router();
 
 router.post("/create", verifyToken, create);
-// router.get("/getposts", getposts);
-// router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
-// router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
+router.get("/getposts", getposts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
 export default router;
